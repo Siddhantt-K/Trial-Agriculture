67d8845331397c1cc4a428b3e4e004510f863789
Siddhantt-K <87244972+Siddhantt-K@users.noreply.github.com>
2022-07-22 16:17:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,32 +12,34 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-img = Image.open("D:/Softwares/DS/Project/Optimising Agriculture/Final Files/assets/icon.png") 
+img = Image.open("assets/icon.png") 
 
 st.set_page_config(page_title="Optimizing Agriculture Web App", 
                    page_icon= img,
                    layout='centered')
 
-data = pd.read_csv("D:/Softwares/DS/Project/Optimising Agriculture/Final Files/dataset/data.csv")
+data = pd.read_csv("dataset/data.csv")
 X = data.iloc[:,0:-1]
 y = data.iloc[:,-1]
 
-model = open("D:/Softwares/DS/Project/Optimising Agriculture/Final Files/Agriculture_XGBoost_Model.pkl", 'rb')
+model = open("Agriculture_XGBoost_Model.pkl", 'rb')
 classifier = pickle.load(model)
 
-home_img = Image.open("D:/Softwares/DS/Project/Optimising Agriculture/Final Files/assets/npk.jpg")
+home_img = Image.open("assets/npk.jpg")
 
 # def open_browser():
 #    webbrowser.open_new("http://127.0.0.1:8501")
-                    
+
+@st.cache(suppress_st_warning=True)                    
 def main():
     
     #option = st.sidebar.selectbox('Options',('Home','Know Your Crop'))
     #st.header(option)
     
-    option = option_menu(menu_title=None,
-                         options=['Home', 'Data Info', 'Visualization', 'Know Your Crop'],
+    option = option_menu(None,
+                         ['Home', 'Data Info', 'Visualization', 'Know Your Crop'],
                          icons=['house-fill', 'info-circle-fill','bar-chart-line-fill', 'caret-right-fill'],
+                         default_index=0,
                          orientation='horizontal',
                          styles={
                                  'Container':{'padding':'0!important', 'background-color':'#fafafa'},
@@ -101,10 +103,11 @@ def main():
             st.subheader('Histogram')
             st.info("If error, try reloading the app.")
             column_dist_plot = st.selectbox("Choose a feature", data.columns[:-1].tolist())
-            fig = sns.distplot(data[column_dist_plot])
+            fig = sns.histplot(data[column_dist_plot])
             st.pyplot()
                                    
     if option == 'Know Your Crop':
+        st.subheader("Fill Below Entries & Get To Know Your Crop")
         
         Nitrogen = st.number_input('Nitrogen', 0, 140)
         Phosphorous = st.number_input('Phosphourus', 5, 145)
