bee84908b1a55d3165c499e9a7d9126632bf3b7b
Siddhantt-K <87244972+Siddhantt-K@users.noreply.github.com>
2022-07-22 22:26:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-img = Image.open("assets/icon.png") 
+img = Image.open("icon.png") 
 
 st.set_page_config(page_title="Optimizing Agriculture Web App", 
                    page_icon= img,
                    layout='centered')
 
-data = pd.read_csv("dataset/data.csv")
+data = pd.read_csv("data.csv")
 X = data.iloc[:,0:-1]
 y = data.iloc[:,-1]
 
 model = open("Agriculture_XGBoost_Model.pkl", 'rb')
 classifier = pickle.load(model)
 
-home_img = Image.open("assets/npk.jpg")
+home_img = Image.open("npk.jpg")
 
 # def open_browser():
 #    webbrowser.open_new("http://127.0.0.1:8501")
